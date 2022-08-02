f407e28b7b5d6b60463971bcc4509778748c45b0
Nate Hessler <natehessler@gmail.com>
2022-08-02 12:11:14 -0500

    insomnia

--- server/routers/pizzas.js
+++ server/routers/pizzas.js
@@ -14,5 +14,56 @@ router.post("/", (request, response) => {
 
 });
 
+// Get (read) all records from the collection
+router.get("/", (request, response) => {
+
+  Pizza.find({}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+
+});
+
+// Get a single record by ID using a query parameter
+router.get("/:id", (request, response) => {
+
+  Pizza.findById(request.params.id, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+
+});
+
+router.delete("/:id", (request, response) => {
+  Pizza.findByIdAndRemove(request.params.id, {}, (error, record) => {
+    if (error) return response.status(500).json(error);
+    return response.json(record);
+  });
+});
+
+router.put("/:id", (request, response) => {
+  const body = request.body;
+  Pizza.findByIdAndUpdate(
+    request.params.id,
+    {
+      $set: {
+        // Take note that the customer is not included, so it can't
+        crust: body.crust,
+        cheese: body.cheese,
+        sauce: body.sauce,
+        toppings: body.toppings
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    },
+    (error, record) => {
+      if (error) return response.status(500).json(error);
+      return response.json(record);
+    }
+  );
+});
+
 module.exports = router;
 
